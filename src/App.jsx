62b1e4992bad5a5af8b05f60bca2d86c808b7286
Gulunsal <gulunsal@example.com>
2024-11-08 19:04:27 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { nanoid } from 'nanoid';
 import ContactForm from './components/ContactForm/ContactForm';
 import ContactList from './components/ContactList/ContactList';
 import SearchBox from './components/SearchBox/SearchBox';
-import styles from './App.module.css';
+import styles from './App.css';
 
 function App() {
   const [contacts, setContacts] = useState(() => {
